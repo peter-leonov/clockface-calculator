30e3e43a7f8070c0049168268d01690525426605
Peter Leonov <gojpeg@gmail.com>
2012-05-20 23:06:21 +0400

    reload on tap

--- calculator.js
+++ calculator.js
@@ -1,18 +1,1 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-<meta charset="utf-8">
-<title>Считалочка времени</title>
-<link rel="apple-touch-icon" href="apple-touch-icon.png">
-<meta name="viewport" content="user-scalable=no, initial-scale=1.0, width=device-width">
-<meta name="apple-mobile-web-app-capable" content="yes">
-<meta name="apple-mobile-web-app-status-bar-style" content="black">
-<meta name="format-detection" content="telephone=no">
-<link rel="stylesheet" href="calculator.css">
-html { background-color: #000; }
-</style>
-<body>
-
-
-
-<script src="calculator.js"></script>
+document.addEventListener('touchstart', function (e) { window.location.reload(true) }, false)
