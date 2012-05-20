44b8008d9754c2b12ac0354a6a3f2304917e33c1
Peter Leonov <gojpeg@gmail.com>
2012-05-20 21:53:33 +0400

    split

--- calculator.js
+++ calculator.js
@@ -2,13 +2,15 @@
 <html lang="ru">
 <head>
 <meta charset="utf-8">
-<title>Считалочка</title>
+<title>Считалочка времени</title>
 <link rel="apple-touch-icon" href="apple-touch-icon.png">
 <meta name="viewport" content="user-scalable=no, initial-scale=1.0, width=device-width">
 <meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="apple-mobile-web-app-status-bar-style" content="black">
 <meta name="format-detection" content="telephone=no">
 <link rel="stylesheet" href="calculator.css">
+html { background-color: #000; }
+</style>
 <body>
 
 
