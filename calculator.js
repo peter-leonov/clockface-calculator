ae39d87c4bb4de1f1e259ed4109ec6bef1d53545
Peter Leonov <gojpeg@gmail.com>
2012-05-21 00:04:09 +0400

    working time result

--- calculator.js
+++ calculator.js
@@ -21,17 +21,39 @@ document.addEventListener('touchstart', touchstart, false)
 
 
 
-var lastHour = null
-function hour (e)
+function bindButtons (nodes, cb)
 {
-	if (lastHour)
-		lastHour.classList.remove('selected')
-	this.classList.toggle('selected')
-	lastHour = this
+	var last = null
+	function tap (e)
+	{
+		if (last)
+			last.classList.remove('selected')
+		this.classList.toggle('selected')
+		last = this
+		cb(this.getAttribute('data-value'))
+	}
+	
+	for (var i = 0, il = nodes.length; i < il; i++)
+		nodes[i].addEventListener('touchstart', tap, false)
 }
 
-var hours = $$('.time-select .hours .button')
-for (var i = 0, il = hours.length; i < il; i++)
-	hours[i].addEventListener('touchstart', hour, false)
+var nodes =
+{
+	hours: $('.time-result .hours').firstChild,
+	minutes: $('.time-result .minutes').firstChild
+}
+
+function hours (v)
+{
+	nodes.hours.nodeValue = v
+}
+
+function minutes (v)
+{
+	nodes.minutes.nodeValue = v
+}
+
+bindButtons($$('.time-select .hours .button'), hours)
+bindButtons($$('.time-select .minutes .button'), minutes)
 
 })();
