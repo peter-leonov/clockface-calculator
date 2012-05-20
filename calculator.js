860dd2c7ab00c9efe6960f12af8ca17d4d1cf802
Peter Leonov <gojpeg@gmail.com>
2012-05-20 23:40:19 +0400

    very close to my imagination

--- calculator.js
+++ calculator.js
@@ -1,1 +1,22 @@
-document.addEventListener('touchstart', function (e) { window.location.reload(true) }, false)
+;(function(){
+
+function touchstart (e)
+{
+	e.preventDefault()
+	window.location.reload(true)
+}
+document.addEventListener('touchstart', touchstart, false)
+
+function tap (e)
+{
+	e.preventDefault()
+	e.stopPropagation()
+	this.classList.toggle('selected')
+}
+
+var buttons = document.querySelectorAll('.time-select .button')
+for (var i = 0, il = buttons.length; i < il; i++)
+	buttons[i].addEventListener('touchstart', tap, false)
+
+})();
+
